1441a77ab57a238cf3593d38fc0f5a27d087b13c
Ziv Izhar <ziv.i@sealsecurity.io>
2024-02-26 11:38:27 +0200

    remove if

--- packaging/build/appveyor.ps1
+++ packaging/build/appveyor.ps1
@@ -40,9 +40,7 @@ Function Bootstrap() {
     
     Write-Output "cloning libyaml from $libyaml_repo_url / $libyaml_refspec"
 
-    If(-not $(Test-Path .\libyaml)) {
-        git clone -b $libyaml_refspec $libyaml_repo_url 2>&1
-    }
+    git clone -b $libyaml_refspec $libyaml_repo_url 2>&1
 }
 
 Function Build-Wheel($python_path) {
